a76a0828def2eaaf2f1da19cca135b3c68a1a5bb
UnusualEgg <shotgun282006@gmail.com>
2024-05-20 17:40:55 +0000

    initial

---
+++ build.rs
@@ -0,0 +1,5 @@
+fn main() {
+    // #[cfg(target_os = "emscripten")]
+    //println!("cargo:rustc-link-arg=--allow-undefined");
+    //println!("cargo::rustc-link-arg=-sERROR_ON_UNDEFINED_SYMBOLS=0");
+}
