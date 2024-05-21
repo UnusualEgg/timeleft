efc9905d2ee23da586a6d9219458178c59fc31e1
UnusualEgg <shotgun282006@gmail.com>
2024-05-21 15:57:57 +0000

    removing excess remaining files

--- build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    // #[cfg(target_os = "emscripten")]
-    //println!("cargo:rustc-link-arg=--allow-undefined");
-    //println!("cargo::rustc-link-arg=-sERROR_ON_UNDEFINED_SYMBOLS=0");
-}
